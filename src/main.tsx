c1618df7cd5c5e8ae793575d5cdecaae04e707bb
Abdul Aziz <abdulazizmishabhudin@gmail.com>
2025-01-30 17:57:53 +0700

    chore: :heavy_plus_sign: init TanStack Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import Dashboard from "./App.tsx";
+import { routeTree } from "./routeTree.gen";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<Dashboard />
-	</StrictMode>,
-);
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+	throw new Error("Root element not found");
+}
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
---
+++ src/routes/__root.tsx
@@ -0,0 +1,23 @@
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { Suspense } from "react";
+import { TanStackRouterDevtools } from "../components/TanStackRouterDevtools.tsx";
+
+export const Route = createRootRoute({
+	component: () => (
+		<>
+			<div className="p-2 flex gap-2">
+				<Link to="/" className="[&.active]:font-bold">
+					Home
+				</Link>{" "}
+				<Link to="/about" className="[&.active]:font-bold">
+					About
+				</Link>
+			</div>
+			<hr />
+			<Outlet />
+			<Suspense>
+				<TanStackRouterDevtools />
+			</Suspense>
+		</>
+	),
+});
