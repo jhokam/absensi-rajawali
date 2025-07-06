2daa1f34a56def5971c962e3c12dd9b76492d27f
jhokam <abdulazizmishabhudin@gmail.com>
2025-07-06 22:04:59 +0700

    feat: :sparkles: add generus create and update page

---
+++ src/routes/admin/_admin/generus/create.tsx
@@ -0,0 +1,377 @@
+import { useForm } from "@tanstack/react-form";
+import { useMutation } from "@tanstack/react-query";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import Button from "@/components/Button";
+import TextError from "@/components/TextError";
+import ThemedInput from "@/components/ThemedInput";
+import ThemedLink from "@/components/ThemedLink";
+import ThemedSelect from "@/components/ThemedSelect";
+import {
+	jenisKelaminOptions,
+	jenjangOptions,
+	kelompokOptions,
+	keteranganOptions,
+	pendidikanTerakhirOptions,
+	sambungOptions,
+} from "@/constants";
+import {
+	type GenerusRequest,
+	type GenerusResponse,
+	generusSchema,
+} from "@/types/generus";
+import { api } from "@/utils/api";
+import { useAlert } from "@/utils/useAlert";
+
+export const Route = createFileRoute("/admin/_admin/generus/create")({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	const { setAlert } = useAlert();
+
+	const { mutateAsync } = useMutation<GenerusResponse, Error, GenerusRequest>({
+		mutationFn: (data: GenerusRequest) => {
+			return api("/generus", {
+				method: "POST",
+				body: JSON.stringify(data),
+			});
+		},
+		onSuccess: (data) => {
+			setAlert(data.message, "success");
+			redirect({ to: "/admin/generus" });
+		},
+		onError: (error) => {
+			setAlert(error.message, "error");
+		},
+	});
+
+	const form = useForm({
+		defaultValues: {
+			nama: "",
+			jenis_kelamin: "Laki_Laki",
+			tempat_lahir: "",
+			tanggal_lahir: new Date(),
+			jenjang: "Paud",
+			nomer_whatsapp: "",
+			pendidikan_terakhir: "PAUD",
+			nama_orang_tua: "",
+			nomer_whatsapp_orang_tua: "",
+			alamat_tempat_tinggal: "",
+			keterangan: "Pendatang",
+			alamat_asal: "",
+			sambung: "Tidak_Aktif",
+			kelompok_id: "",
+		},
+		onSubmit: async ({ value }) => {
+			await mutateAsync(value as GenerusRequest);
+		},
+		validators: {
+			onSubmit: generusSchema,
+		},
+	});
+
+	return (
+		<div className="w-full">
+			<h1 className="text-2xl font-bold mb-6 text-gray-800">
+				Buat Data Generus
+			</h1>
+
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					form.handleSubmit();
+				}}
+				className="space-y-4">
+				<div className="space-y-4">
+					<form.Field
+						name="nama"
+						children={(field) => (
+							<>
+								<ThemedInput
+									label="Nama"
+									variant="secondary"
+									htmlFor={field.name}
+									type="text"
+									name={field.name}
+									id={field.name}
+									value={field.state.value}
+									onBlur={field.handleBlur}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="John Doe"
+									required={true}
+									className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+								/>
+								<TextError field={field} />
+							</>
+						)}
+					/>
+
+					<form.Field
+						name="jenis_kelamin"
+						children={(field) => (
+							<div className="space-y-1">
+								<ThemedSelect
+									name={field.name}
+									label="Jenis Kelamin"
+									options={jenisKelaminOptions}
+									field={field}
+									placeholder="Pilih Jenis Kelamin"
+									required={true}
+								/>
+							</div>
+						)}
+					/>
+
+					<form.Field
+						name="tempat_lahir"
+						children={(field) => (
+							<>
+								<ThemedInput
+									label="Tempat Lahir"
+									htmlFor={field.name}
+									type="text"
+									name={field.name}
+									id={field.name}
+									value={field.state.value}
+									onBlur={field.handleBlur}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="John Doe"
+									required={true}
+									className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+								/>
+								<TextError field={field} />
+							</>
+						)}
+					/>
+
+					<form.Field
+						name="tanggal_lahir"
+						children={(field) => (
+							<>
+								<ThemedInput
+									label="Tanggal Lahir"
+									htmlFor={field.name}
+									type="date"
+									name={field.name}
+									id={field.name}
+									value={field.state.value?.toISOString().split("T")[0] || ""}
+									onBlur={field.handleBlur}
+									onChange={(e) =>
+										field.handleChange(e.target.valueAsDate || new Date())
+									}
+									placeholder="John Doe"
+									required={true}
+									className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+								/>
+								<TextError field={field} />
+							</>
+						)}
+					/>
+
+					<form.Field
+						name="jenjang"
+						children={(field) => (
+							<div className="space-y-1">
+								<ThemedSelect
+									name={field.name}
+									label="Jenjang"
+									options={jenjangOptions}
+									field={field}
+									placeholder="Pilih Jenjang"
+									required={true}
+								/>
+							</div>
+						)}
+					/>
+
+					<form.Field
+						name="nomer_whatsapp"
+						children={(field) => (
+							<>
+								<ThemedInput
+									label="Nomor WhatsApp"
+									htmlFor={field.name}
+									type="text"
+									name={field.name}
+									id={field.name}
+									value={field.state.value}
+									onBlur={field.handleBlur}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="08123456789"
+									required={true}
+									className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+								/>
+								<TextError field={field} />
+							</>
+						)}
+					/>
+
+					<form.Field
+						name="pendidikan_terakhir"
+						children={(field) => (
+							<div className="space-y-1">
+								<ThemedSelect
+									name={field.name}
+									label="Pendidikan Terakhir"
+									options={pendidikanTerakhirOptions}
+									field={field}
+									placeholder="Pilih Pendidikan Terakhir"
+									required={true}
+								/>
+							</div>
+						)}
+					/>
+
+					<form.Field
+						name="nama_orang_tua"
+						children={(field) => (
+							<>
+								<ThemedInput
+									label="Nama Orang Tua"
+									htmlFor={field.name}
+									type="text"
+									name={field.name}
+									id={field.name}
+									value={field.state.value}
+									onBlur={field.handleBlur}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="John Doe"
+									required={true}
+									className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+								/>
+								<TextError field={field} />
+							</>
+						)}
+					/>
+
+					<form.Field
+						name="nomer_whatsapp_orang_tua"
+						children={(field) => (
+							<>
+								<ThemedInput
+									label="Nomor WhatsApp Orang Tua"
+									htmlFor={field.name}
+									type="text"
+									name={field.name}
+									id={field.name}
+									value={field.state.value}
+									onBlur={field.handleBlur}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="08123456789"
+									required={true}
+									className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+								/>
+								<TextError field={field} />
+							</>
+						)}
+					/>
+
+					<form.Field
+						name="sambung"
+						children={(field) => (
+							<div className="space-y-1">
+								<ThemedSelect
+									name={field.name}
+									label="Sambung"
+									options={sambungOptions}
+									field={field}
+									placeholder="Pilih Sambung"
+									required={true}
+								/>
+							</div>
+						)}
+					/>
+
+					<form.Field
+						name="alamat_tempat_tinggal"
+						children={(field) => (
+							<>
+								<ThemedInput
+									label="Alamat Tempat Tinggal"
+									htmlFor={field.name}
+									type="text"
+									name={field.name}
+									id={field.name}
+									value={field.state.value}
+									onBlur={field.handleBlur}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="Jl. Madukoro No. 1"
+									required={true}
+									className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+								/>
+								<TextError field={field} />
+							</>
+						)}
+					/>
+
+					<form.Field
+						name="keterangan"
+						children={(field) => (
+							<div className="space-y-1">
+								<ThemedSelect
+									name={field.name}
+									label="Keterangan"
+									options={keteranganOptions}
+									field={field}
+									placeholder="Pilih Keterangan"
+									required={true}
+								/>
+							</div>
+						)}
+					/>
+
+					<form.Field
+						name="alamat_asal"
+						children={(field) => (
+							<>
+								<ThemedInput
+									label="Alamat Asal"
+									htmlFor={field.name}
+									type="text"
+									name={field.name}
+									id={field.name}
+									value={field.state.value}
+									onBlur={field.handleBlur}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="Jl. Madukoro No. 1"
+									required={true}
+									className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+								/>
+								<TextError field={field} />
+							</>
+						)}
+					/>
+
+					<form.Field
+						name="kelompok_id"
+						children={(field) => (
+							<div className="space-y-1">
+								<ThemedSelect
+									name={field.name}
+									label="Kelompok"
+									options={kelompokOptions}
+									field={field}
+									placeholder="Pilih Kelompok"
+									required={true}
+								/>
+							</div>
+						)}
+					/>
+				</div>
+
+				<div className="flex justify-end space-x-4 mt-6">
+					<ThemedLink to="/admin/generus">Close</ThemedLink>
+					<form.Subscribe
+						selector={(state) => [state.canSubmit, state.isSubmitting]}
+						children={([canSubmit, isSubmitting]) => (
+							<Button type="submit" disabled={!canSubmit}>
+								{isSubmitting ? "Memproses..." : "Submit"}
+							</Button>
+						)}
+					/>
+				</div>
+			</form>
+		</div>
+	);
+}
---
+++ src/routes/admin/_admin/generus/update.$id.tsx
@@ -0,0 +1,382 @@
+import { useForm } from "@tanstack/react-form";
+import { useMutation } from "@tanstack/react-query";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import Button from "@/components/Button";
+import TextError from "@/components/TextError";
+import ThemedInput from "@/components/ThemedInput";
+import ThemedLink from "@/components/ThemedLink";
+import ThemedSelect from "@/components/ThemedSelect";
+import {
+	jenisKelaminOptions,
+	jenjangOptions,
+	kelompokOptions,
+	keteranganOptions,
+	pendidikanTerakhirOptions,
+	sambungOptions,
+} from "@/constants";
+import {
+	type GenerusRequest,
+	type GenerusResponse,
+	generusSchema,
+} from "@/types/generus";
+import { api } from "@/utils/api";
+import { useAlert } from "@/utils/useAlert";
+
+export const Route = createFileRoute("/admin/_admin/generus/update/$id")({
+	component: RouteComponent,
+	loader: async ({params}): Promise<GenerusResponse> => api(`/generus/${params.id}`)
+});
+
+function RouteComponent() {
+	const { data } = Route.useLoaderData();
+	console.log(data)
+
+	const { setAlert } = useAlert();
+
+	const { mutateAsync } = useMutation<GenerusResponse, Error, GenerusRequest>({
+		mutationFn: async (request) => {
+			return api(`/generus/${data.id}`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(request),
+			})
+		},
+		onSuccess: (data) => {
+			setAlert(data.message, "success");
+			redirect({ to: "/admin/generus" });
+		},
+		onError: (error) => {
+			setAlert(error.message, "error");
+		},
+	});
+
+	const form = useForm({
+		defaultValues: {
+			nama: data.nama,
+			jenis_kelamin: data.jenis_kelamin,
+			tempat_lahir: data.tempat_lahir,
+			tanggal_lahir: data.tanggal_lahir,
+			jenjang: data.jenjang,
+			nomer_whatsapp: data.nomer_whatsapp,
+			pendidikan_terakhir: data.pendidikan_terakhir,
+			nama_orang_tua: data.nama_orang_tua,
+			nomer_whatsapp_orang_tua: data.nomer_whatsapp_orang_tua,
+			alamat_tempat_tinggal: data.alamat_tempat_tinggal,
+			keterangan: data.keterangan,
+			alamat_asal: data.alamat_asal,
+			sambung: data.sambung,
+			kelompok_id: data.kelompok_id,
+		},
+		onSubmit: async ({ value }) => {
+			await mutateAsync(value);
+		},
+		validators: {
+			onSubmit: generusSchema,
+		},
+	});
+
+	return (
+			<div className="w-full">
+				<h1 className="text-2xl font-bold mb-6 text-gray-800">Update Data</h1>
+
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
+						form.handleSubmit();
+					}}
+					className="space-y-4">
+					<div className="space-y-4">
+						<form.Field
+							name="nama"
+							children={(field) => (
+								<>
+									<ThemedInput
+										label="Nama"
+										variant="secondary"
+										htmlFor={field.name}
+										type="text"
+										name={field.name}
+										id={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="John Doe"
+										required={true}
+										className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+									/>
+									<TextError field={field} />
+								</>
+							)}
+						/>
+
+						<form.Field
+							name="jenis_kelamin"
+							children={(field) => (
+								<div className="space-y-1">
+									<ThemedSelect
+										name={field.name}
+										label="Jenis Kelamin"
+										options={jenisKelaminOptions}
+										field={field}
+										placeholder="Pilih Jenis Kelamin"
+										required={true}
+									/>
+								</div>
+							)}
+						/>
+
+						<form.Field
+							name="tempat_lahir"
+							children={(field) => (
+								<>
+									<ThemedInput
+										label="Tempat Lahir"
+										htmlFor={field.name}
+										type="text"
+										name={field.name}
+										id={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="John Doe"
+										required={true}
+										className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+									/>
+									<TextError field={field} />
+								</>
+							)}
+						/>
+
+						<form.Field
+							name="tanggal_lahir"
+							children={(field) => (
+								<>
+									<ThemedInput
+										label="Tanggal Lahir"
+										htmlFor={field.name}
+										type="date"
+										name={field.name}
+										id={field.name}
+										value={String(field.state.value)}
+										onBlur={field.handleBlur}
+										onChange={(e) =>
+											field.handleChange(new Date(e.target.value))
+										}
+										placeholder="John Doe"
+										required={true}
+										className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+									/>
+									<TextError field={field} />
+								</>
+							)}
+						/>
+
+						<form.Field
+							name="jenjang"
+							children={(field) => (
+								<div className="space-y-1">
+									<ThemedSelect
+										name={field.name}
+										label="Jenjang"
+										options={jenjangOptions}
+										field={field}
+										placeholder="Pilih Jenjang"
+										required={true}
+									/>
+								</div>
+							)}
+						/>
+
+						<form.Field
+							name="nomer_whatsapp"
+							children={(field) => (
+								<>
+									<ThemedInput
+										label="Nomor WhatsApp"
+										htmlFor={field.name}
+										type="text"
+										name={field.name}
+										id={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="08123456789"
+										required={true}
+										className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+									/>
+									<TextError field={field} />
+								</>
+							)}
+						/>
+
+						<form.Field
+							name="pendidikan_terakhir"
+							children={(field) => (
+								<div className="space-y-1">
+									<ThemedSelect
+										name={field.name}
+										label="Pendidikan Terakhir"
+										options={pendidikanTerakhirOptions}
+										field={field}
+										placeholder="Pilih Pendidikan Terakhir"
+										required={true}
+									/>
+								</div>
+							)}
+						/>
+
+						<form.Field
+							name="nama_orang_tua"
+							children={(field) => (
+								<>
+									<ThemedInput
+										label="Nama Orang Tua"
+										htmlFor={field.name}
+										type="text"
+										name={field.name}
+										id={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="John Doe"
+										required={true}
+										className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+									/>
+									<TextError field={field} />
+								</>
+							)}
+						/>
+
+						<form.Field
+							name="nomer_whatsapp_orang_tua"
+							children={(field) => (
+								<>
+									<ThemedInput
+										label="Nomor WhatsApp Orang Tua"
+										htmlFor={field.name}
+										type="text"
+										name={field.name}
+										id={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="08123456789"
+										required={true}
+										className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+									/>
+									<TextError field={field} />
+								</>
+							)}
+						/>
+
+						<form.Field
+							name="sambung"
+							children={(field) => (
+								<div className="space-y-1">
+									<ThemedSelect
+										name={field.name}
+										label="Sambung"
+										options={sambungOptions}
+										field={field}
+										placeholder="Pilih Sambung"
+										required={true}
+									/>
+								</div>
+							)}
+						/>
+
+						<form.Field
+							name="alamat_tempat_tinggal"
+							children={(field) => (
+								<>
+									<ThemedInput
+										label="Alamat Tempat Tinggal"
+										htmlFor={field.name}
+										type="text"
+										name={field.name}
+										id={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="Jl. Madukoro No. 1"
+										required={true}
+										className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+									/>
+									<TextError field={field} />
+								</>
+							)}
+						/>
+
+						<form.Field
+							name="keterangan"
+							children={(field) => (
+								<div className="space-y-1">
+									<ThemedSelect
+										name={field.name}
+										label="Keterangan"
+										options={keteranganOptions}
+										field={field}
+										placeholder="Pilih Keterangan"
+										required={true}
+									/>
+								</div>
+							)}
+						/>
+
+						<form.Field
+							name="alamat_asal"
+							children={(field) => (
+								<>
+									<ThemedInput
+										label="Alamat Asal"
+										htmlFor={field.name}
+										type="text"
+										name={field.name}
+										id={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="Jl. Madukoro No. 1"
+										required={true}
+										className="w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
+									/>
+									<TextError field={field} />
+								</>
+							)}
+						/>
+
+						<form.Field
+							name="kelompok_id"
+							children={(field) => (
+								<div className="space-y-1">
+									<ThemedSelect
+										name={field.name}
+										label="Kelompok"
+										options={kelompokOptions}
+										field={field}
+										placeholder="Pilih Kelompok"
+										required={true}
+									/>
+								</div>
+							)}
+						/>
+					</div>
+
+					<div className="flex justify-end space-x-4 mt-6">
+						<ThemedLink to="/admin/generus">Close</ThemedLink>
+						<form.Subscribe
+							selector={(state) => [state.canSubmit, state.isSubmitting]}
+							children={([canSubmit, isSubmitting]) => (
+								<Button type="submit" disabled={!canSubmit}>
+									{isSubmitting ? "Memproses..." : "Submit"}
+								</Button>
+							)}
+						/>
+					</div>
+				</form>
+			</div>
+	)
+}
